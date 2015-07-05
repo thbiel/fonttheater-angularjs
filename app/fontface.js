8bd18c18193cc7e0ddb08837b3120b884a0aefee
Tor Håvard Bieltvedt <th@bieltvedt.net>
2015-07-05 12:58:43 +0200

    Update location path on font change.

--- app/fontface.js
+++ app/fontface.js
@@ -21,7 +21,7 @@ app.config(
     }]
 );
 
-app.controller('FontController', function ($scope, $routeParams, $route, fontLibraryService, colorSchemes) {
+app.controller('FontController', function ($scope, $routeParams, $route, $location, fontLibraryService, colorSchemes) {
 
     //fontLibraryService.load(function (fontLib) {
     //    $scope.fontLibrary = fontLib;
@@ -36,17 +36,21 @@ app.controller('FontController', function ($scope, $routeParams, $route, fontLib
     $scope.fontCollections = $scope.fontLibrary.fontCollections;
 
     $scope.selectFontById = function(fontId) {
+        goToUrlForFont(fontId);
+    };
+
+    function currentFont(fontId) {
         var font = $scope.fontLibrary.fontsById[fontId];
         $scope.currentFont = font;
         $scope.currentFontId = font.id;
         $scope.currentFontStyle = fontStyle(font);
-    };
+    }
 
     $scope.selectFontCollectionById = function(fontCollectionId) {
         var fontCollection = $scope.fontLibrary.fontCollectionsById[fontCollectionId];
         $scope.currentFontCollection = fontCollection.fonts;
         $scope.currentFontCollectionId = fontCollection.id;
-        $scope.selectFontById($scope.currentFontCollection[0].id);
+        currentFont($scope.currentFontCollection[0].id);
     };
 
     $scope.selectFontCollectionById($scope.fontCollections[0].id);
@@ -54,16 +58,21 @@ app.controller('FontController', function ($scope, $routeParams, $route, fontLib
     $scope.selectPreviousFont = function() {
         var index = $scope.currentFontCollection.indexOf($scope.currentFont);
         if (index > 0) {
-            $scope.selectFontById($scope.currentFontCollection[index - 1].id);
+            goToUrlForFont($scope.currentFontCollection[index - 1].id)
         }
     };
 
     $scope.selectNextFont = function() {
         var index = $scope.currentFontCollection.indexOf($scope.currentFont);
         if (index < ($scope.currentFontCollection.length - 1)) {
-            $scope.selectFontById($scope.currentFontCollection[index + 1].id);
+            goToUrlForFont($scope.currentFontCollection[index + 1].id)
         }
     };
+
+    function goToUrlForFont(fontId) {
+        var page = $location.path().split('/')[1];
+        $location.path('/' + page + '/' + $scope.selectedColorScheme.id + '/' +  fontId)
+    }
 
     $scope.keyPressed = function(event) {
         if (event.which == 37) {
@@ -78,21 +87,21 @@ app.controller('FontController', function ($scope, $routeParams, $route, fontLib
     $scope.$on('$routeChangeSuccess', function() {
         var colorScheme = $routeParams['colorScheme'];
         if (!colorScheme) {
-            colorScheme = colorSchemes.colorSchemesById[0];
+            colorScheme = colorSchemes.colorSchemes[0].id;
         }
+
         selectColorScheme(colorScheme);
 
         var fontParam = $routeParams['font'];
-        console.log("fontParam: " + fontParam);
+
         var fontId;
         if (fontParam) {
             fontId = fontParam;
         } else {
             fontId = $scope.currentFontCollection[0].id;
         }
-        console.log("fontId: " + fontParam);
-        console.log("$scope.currentFontCollection[0].id: " + $scope.currentFontCollection[0].id);
-        $scope.selectFontById(fontId);
+
+        currentFont(fontId);
     });
 
     function selectColorScheme(id) {
