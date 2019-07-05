6d7a6450ae000d20f81d2d6ac0873b917a4cc875
Tor Håvard Bieltvedt <th@bieltvedt.net>
2019-07-05 10:36:50 +0200

    New commit...

--- colorschemes/colorschemes.js
+++ colorschemes/colorschemes.js
@@ -11,6 +11,11 @@ angular.module('colorSchemes', [])
 
 var colorSchemes = [
     {id: 'Blue'    , primaryBackgroundColor: '#008DD0', shadowBackgroundColor: '#0076AF', primaryTextColor: '#00476E', secondaryTextColor: 'white'},
+    {id: 'Slate 1'   , primaryBackgroundColor: '#263238', shadowBackgroundColor: '#73A643', primaryTextColor: '#FFCB6B', secondaryTextColor: 'white'},
+    {id: 'Slate 2'   , primaryBackgroundColor: '#263238', shadowBackgroundColor: '#73A643', primaryTextColor: '#C3E88D', secondaryTextColor: 'white'},
+    {id: 'Slate 3'   , primaryBackgroundColor: '#263238', shadowBackgroundColor: '#73A643', primaryTextColor: '#C792EA', secondaryTextColor: 'white'},
+    {id: 'Slate 4'   , primaryBackgroundColor: '#263238', shadowBackgroundColor: '#73A643', primaryTextColor: '#5CACFF', secondaryTextColor: 'white'},
+    {id: 'Slate 5'   , primaryBackgroundColor: '#263238', shadowBackgroundColor: '#73A643', primaryTextColor: '#FF4965', secondaryTextColor: 'white'},
     {id: 'Green'   , primaryBackgroundColor: '#8BC752', shadowBackgroundColor: '#73A643', primaryTextColor: '#00476E', secondaryTextColor: 'white'},
     {id: 'Orange'  , primaryBackgroundColor: '#F58221', shadowBackgroundColor: '#CA6C16', primaryTextColor: '#00476E', secondaryTextColor: 'white'},
     {id: 'Pink'    , primaryBackgroundColor: '#EE1451', shadowBackgroundColor: '#C40C42', primaryTextColor: '#00476E', secondaryTextColor: 'white'},
@@ -22,7 +27,7 @@ var colorSchemes = [
     {id: 'Blue 2'  , primaryBackgroundColor: '#487897', shadowBackgroundColor: '#426D87', primaryTextColor: '#00476E', secondaryTextColor: 'white'},
     {id: 'Yellow 2', primaryBackgroundColor: '#FFC107', shadowBackgroundColor: '#FFC107', primaryTextColor: '#00476E', secondaryTextColor: 'white'},
     {id: 'White'   , primaryBackgroundColor: '#FFFFFF', shadowBackgroundColor: '#FFFFFF', primaryTextColor: '#00476E', secondaryTextColor: '#00476E'},
-    {id: 'Oliven'  , primaryBackgroundColor: '#242B33', shadowBackgroundColor: '#242B33'  , primaryTextColor: '#82aa3e', secondaryTextColor: 'white'}
+    {id: 'Oliven'  , primaryBackgroundColor: '#242B33', shadowBackgroundColor: '#242B33', primaryTextColor: '#82aa3e', secondaryTextColor: 'white'}
 ];
 
 var colorSchemesById = mapById(colorSchemes);
